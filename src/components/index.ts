4bb0ac44bca2eae8637417f1e5ddb3709524c411
hamid raza <201445491+alphawolf1994@users.noreply.github.com>
2025-10-14 13:45:40 +0500

    remove extra code

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,37 +1,10 @@
-import Header from "./Header";
-import HeaderMain from "./HeaderMain";
+
 import Button from "./Button";
 import MyTextInput from "./MyTextInput";
-import ErrorText from "./ErrorText";
-import HeaderHome from "./HeaderHome";
-import HeadingText from "./HeadingText";
-import HomeworkCard from "./HomeworkCard";
-import NoticeBoardCard from "./NoticeBoardCard";
-import ModalNavigation from "./ModalNavigation";
-import FeeDetailsCard from "./FeeDetailCard";
-import ExamFeesTab from "./ExamFeesTab";
-import SchoolFeesTab from "./SchoolFeesTab";
-import ActivityFeesTab from "./ActivityFeesTab";
-import ExaminationCard from "./ExaminationCard";
-import MultimediaCard from "./MultimediaCard";
-import MultimediaTab from "./MultimediaTab";
 
 export {
-  Header,
-  HeaderMain,
-  HeaderHome,
-  HeadingText,
-  NoticeBoardCard,
-  HomeworkCard,
-  ExaminationCard,
-  ModalNavigation,
-  ExamFeesTab,
-  SchoolFeesTab,
-  ActivityFeesTab,
-  FeeDetailsCard,
+ 
   Button,
   MyTextInput,
-  ErrorText,
-  MultimediaCard,
-  MultimediaTab,
+  
 };
